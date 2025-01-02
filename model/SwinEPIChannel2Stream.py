594568405a3c7d94f711e068fd21f805f130182d
MyllenaAPrado <myllena.pradoa@gmail.com>
2025-01-02 12:04:05 -0500

    NAT

--- model/SwinEPIChannel2Stream.py
+++ model/SwinEPIChannel2Stream.py
@@ -109,7 +109,7 @@ class IntegratedModelV2(nn.Module):
             for i in range(1)
         ])
 
-        embed_dim = 512
+        embed_dim = 132
         # Adaptive head
         self.head_score = nn.Sequential(
             nn.Linear(embed_dim, embed_dim//2),
